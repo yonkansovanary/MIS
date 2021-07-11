73dbaa247adf3f73a74393f9e1e018807fc58ec4
Yon Kansovanary <yonsovann009@gmail.com>
2021-07-11 15:59:31 +0700

    Add connection and ignore file

--- AssGroupIII/AssGroupIII/Connection.vb
+++ AssGroupIII/AssGroupIII/Connection.vb
@@ -1,6 +1,22 @@
 ﻿
-
+Imports System.Data.SqlClient
 Public Class Connection
-
-
+    Private myConn As SqlConnection
+    Private myCmd As SqlCommand
+    Private myReader As SqlDataReader
+    Private results As String
+    Public Sub sqlConnector()
+        myConn = New SqlConnection("Server= DESKTOP-JTNSKEL; Database= AssignmentGroupIII; Integrated Security=SSPI;")
+        myCmd = myConn.CreateCommand
+        Return
+    End Sub
+    Public Function listAllData(sql As String)
+        Dim data As New DataTable
+        sqlConnector()
+        myCmd.CommandText = sql
+        myConn.Open()
+        myReader = myCmd.ExecuteReader()
+        data.Load(myReader)
+        Return data
+    End Function
 End Class
--- AssGroupIII/AssGroupIII/MainForm.Designer.vb
+++ AssGroupIII/AssGroupIII/MainForm.Designer.vb
@@ -27,16 +27,19 @@ Partial Class MainForm
         Me.Panel2 = New System.Windows.Forms.Panel()
         Me.Panel3 = New System.Windows.Forms.Panel()
         Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.Panel2.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(628, 257)
+        Me.Label1.Location = New System.Drawing.Point(1026, 115)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(91, 24)
+        Me.Label1.Size = New System.Drawing.Size(115, 29)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Welcome"
         '
@@ -45,8 +48,9 @@ Partial Class MainForm
         Me.Panel1.BackColor = System.Drawing.Color.Navy
         Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(200, 703)
+        Me.Panel1.Size = New System.Drawing.Size(267, 865)
         Me.Panel1.TabIndex = 1
         '
         'Panel2
@@ -54,9 +58,10 @@ Partial Class MainForm
         Me.Panel2.BackColor = System.Drawing.SystemColors.AppWorkspace
         Me.Panel2.Controls.Add(Me.Panel3)
         Me.Panel2.Dock = System.Windows.Forms.DockStyle.Top
-        Me.Panel2.Location = New System.Drawing.Point(200, 0)
+        Me.Panel2.Location = New System.Drawing.Point(267, 0)
+        Me.Panel2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(1170, 47)
+        Me.Panel2.Size = New System.Drawing.Size(1560, 58)
         Me.Panel2.TabIndex = 2
         '
         'Panel3
@@ -64,31 +69,45 @@ Partial Class MainForm
         Me.Panel3.BackColor = System.Drawing.SystemColors.Control
         Me.Panel3.Dock = System.Windows.Forms.DockStyle.Top
         Me.Panel3.Location = New System.Drawing.Point(0, 0)
+        Me.Panel3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(1170, 46)
+        Me.Panel3.Size = New System.Drawing.Size(1560, 57)
         Me.Panel3.TabIndex = 3
         '
         'Panel4
         '
         Me.Panel4.BackColor = System.Drawing.Color.DarkBlue
         Me.Panel4.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.Panel4.Location = New System.Drawing.Point(200, 657)
+        Me.Panel4.Location = New System.Drawing.Point(267, 808)
+        Me.Panel4.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Panel4.Name = "Panel4"
-        Me.Panel4.Size = New System.Drawing.Size(1170, 46)
+        Me.Panel4.Size = New System.Drawing.Size(1560, 57)
         Me.Panel4.TabIndex = 3
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(380, 203)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(962, 397)
+        Me.DataGridView1.TabIndex = 4
         '
         'MainForm
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1370, 703)
+        Me.ClientSize = New System.Drawing.Size(1827, 865)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Panel4)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "MainForm"
         Me.Text = "MainForm"
         Me.Panel2.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -99,4 +118,5 @@ Partial Class MainForm
     Friend WithEvents Panel2 As Panel
     Friend WithEvents Panel3 As Panel
     Friend WithEvents Panel4 As Panel
+    Friend WithEvents DataGridView1 As DataGridView
 End Class
