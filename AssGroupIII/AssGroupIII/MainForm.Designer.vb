acff5643a6cc3844369508e40d827b330e7d66bd
Se Has <hassecskh@gmail.com>
2021-07-11 19:25:17 +0700

    Add Sec user panel

--- AssGroupIII/AssGroupIII/MainForm.Designer.vb
+++ AssGroupIII/AssGroupIII/MainForm.Designer.vb
@@ -29,21 +29,54 @@ Partial Class MainForm
         Me.Panel3 = New System.Windows.Forms.Panel()
         Me.Panel4 = New System.Windows.Forms.Panel()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Panel7 = New System.Windows.Forms.Panel()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.Panel8 = New System.Windows.Forms.Panel()
+        Me.Panel9 = New System.Windows.Forms.Panel()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.Panel10 = New System.Windows.Forms.Panel()
+        Me.Panel13 = New System.Windows.Forms.Panel()
+        Me.Panel16 = New System.Windows.Forms.Panel()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.ListUserPanel = New System.Windows.Forms.Panel()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PictureBox10 = New System.Windows.Forms.PictureBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Adminbtn = New System.Windows.Forms.Label()
+        Me.Panel11 = New System.Windows.Forms.Panel()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.AdminLabel = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
         Me.Panel2.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel5.SuspendLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel7.SuspendLayout()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel9.SuspendLayout()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel13.SuspendLayout()
+        Me.Panel16.SuspendLayout()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ListUserPanel.SuspendLayout()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(628, 257)
+        Me.Label1.Location = New System.Drawing.Point(726, 344)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(91, 24)
         Me.Label1.TabIndex = 0
@@ -52,6 +85,10 @@ Partial Class MainForm
         'Panel1
         '
         Me.Panel1.BackColor = System.Drawing.Color.Navy
+        Me.Panel1.Controls.Add(Me.Panel13)
+        Me.Panel1.Controls.Add(Me.Panel9)
+        Me.Panel1.Controls.Add(Me.Panel7)
+        Me.Panel1.Controls.Add(Me.Panel5)
         Me.Panel1.Controls.Add(Me.PictureBox2)
         Me.Panel1.Controls.Add(Me.Label3)
         Me.Panel1.Controls.Add(Me.PictureBox1)
@@ -101,20 +138,13 @@ Partial Class MainForm
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "Inventory System"
         '
-        'FlowLayoutPanel1
-        '
-        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(609, 220)
-        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
-        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(200, 100)
-        Me.FlowLayoutPanel1.TabIndex = 5
-        '
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
         Me.PictureBox1.Location = New System.Drawing.Point(48, 71)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(95, 99)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 6
         Me.PictureBox1.TabStop = False
         '
@@ -123,7 +153,7 @@ Partial Class MainForm
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label3.ForeColor = System.Drawing.Color.White
-        Me.Label3.Location = New System.Drawing.Point(65, 184)
+        Me.Label3.Location = New System.Drawing.Point(65, 182)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(86, 20)
         Me.Label3.TabIndex = 7
@@ -132,21 +162,224 @@ Partial Class MainForm
         'PictureBox2
         '
         Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
-        Me.PictureBox2.Location = New System.Drawing.Point(39, 183)
+        Me.PictureBox2.Location = New System.Drawing.Point(39, 174)
         Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(24, 20)
+        Me.PictureBox2.Size = New System.Drawing.Size(29, 29)
         Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox2.TabIndex = 8
         Me.PictureBox2.TabStop = False
+        '
+        'Panel5
+        '
+        Me.Panel5.Controls.Add(Me.PictureBox3)
+        Me.Panel5.Controls.Add(Me.Panel6)
+        Me.Panel5.Location = New System.Drawing.Point(0, 209)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(200, 50)
+        Me.Panel5.TabIndex = 6
+        '
+        'Panel6
+        '
+        Me.Panel6.Location = New System.Drawing.Point(1, 56)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Size = New System.Drawing.Size(200, 44)
+        Me.Panel6.TabIndex = 7
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(3, 11)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(29, 29)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox3.TabIndex = 9
+        Me.PictureBox3.TabStop = False
+        '
+        'Panel7
+        '
+        Me.Panel7.Controls.Add(Me.PictureBox4)
+        Me.Panel7.Controls.Add(Me.Panel8)
+        Me.Panel7.Location = New System.Drawing.Point(0, 258)
+        Me.Panel7.Name = "Panel7"
+        Me.Panel7.Size = New System.Drawing.Size(200, 50)
+        Me.Panel7.TabIndex = 10
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(3, 11)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(29, 29)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox4.TabIndex = 9
+        Me.PictureBox4.TabStop = False
+        '
+        'Panel8
+        '
+        Me.Panel8.Location = New System.Drawing.Point(1, 56)
+        Me.Panel8.Name = "Panel8"
+        Me.Panel8.Size = New System.Drawing.Size(200, 44)
+        Me.Panel8.TabIndex = 7
+        '
+        'Panel9
+        '
+        Me.Panel9.Controls.Add(Me.PictureBox5)
+        Me.Panel9.Controls.Add(Me.Panel10)
+        Me.Panel9.Location = New System.Drawing.Point(1, 307)
+        Me.Panel9.Name = "Panel9"
+        Me.Panel9.Size = New System.Drawing.Size(200, 50)
+        Me.Panel9.TabIndex = 11
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), System.Drawing.Image)
+        Me.PictureBox5.Location = New System.Drawing.Point(3, 11)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(29, 29)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox5.TabIndex = 9
+        Me.PictureBox5.TabStop = False
+        '
+        'Panel10
+        '
+        Me.Panel10.Location = New System.Drawing.Point(1, 56)
+        Me.Panel10.Name = "Panel10"
+        Me.Panel10.Size = New System.Drawing.Size(200, 44)
+        Me.Panel10.TabIndex = 7
+        '
+        'Panel13
+        '
+        Me.Panel13.BackColor = System.Drawing.Color.Navy
+        Me.Panel13.Controls.Add(Me.Panel11)
+        Me.Panel13.Controls.Add(Me.Panel16)
+        Me.Panel13.Controls.Add(Me.PictureBox9)
+        Me.Panel13.Controls.Add(Me.Label4)
+        Me.Panel13.Controls.Add(Me.PictureBox10)
+        Me.Panel13.Controls.Add(Me.Label5)
+        Me.Panel13.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel13.ForeColor = System.Drawing.Color.White
+        Me.Panel13.Location = New System.Drawing.Point(0, 0)
+        Me.Panel13.Name = "Panel13"
+        Me.Panel13.Size = New System.Drawing.Size(200, 703)
+        Me.Panel13.TabIndex = 12
+        '
+        'Panel16
+        '
+        Me.Panel16.Controls.Add(Me.Adminbtn)
+        Me.Panel16.Controls.Add(Me.PictureBox8)
+        Me.Panel16.Location = New System.Drawing.Point(0, 228)
+        Me.Panel16.Name = "Panel16"
+        Me.Panel16.Size = New System.Drawing.Size(200, 43)
+        Me.Panel16.TabIndex = 6
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.Image = CType(resources.GetObject("PictureBox8.Image"), System.Drawing.Image)
+        Me.PictureBox8.Location = New System.Drawing.Point(14, 7)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(29, 29)
+        Me.PictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox8.TabIndex = 9
+        Me.PictureBox8.TabStop = False
+        '
+        'ListUserPanel
+        '
+        Me.ListUserPanel.Controls.Add(Me.DataGridView1)
+        Me.ListUserPanel.Location = New System.Drawing.Point(200, 100)
+        Me.ListUserPanel.Name = "ListUserPanel"
+        Me.ListUserPanel.Size = New System.Drawing.Size(1170, 551)
+        Me.ListUserPanel.TabIndex = 7
+        Me.ListUserPanel.Visible = False
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.Image = CType(resources.GetObject("PictureBox9.Image"), System.Drawing.Image)
+        Me.PictureBox9.Location = New System.Drawing.Point(39, 174)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(29, 29)
+        Me.PictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox9.TabIndex = 8
+        Me.PictureBox9.TabStop = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(65, 182)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(86, 20)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Lisa Khim"
+        '
+        'PictureBox10
+        '
+        Me.PictureBox10.Image = CType(resources.GetObject("PictureBox10.Image"), System.Drawing.Image)
+        Me.PictureBox10.Location = New System.Drawing.Point(48, 71)
+        Me.PictureBox10.Name = "PictureBox10"
+        Me.PictureBox10.Size = New System.Drawing.Size(95, 99)
+        Me.PictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox10.TabIndex = 6
+        Me.PictureBox10.TabStop = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(15, 13)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(168, 24)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Inventory System"
+        '
+        'Adminbtn
+        '
+        Me.Adminbtn.AutoSize = True
+        Me.Adminbtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Adminbtn.ForeColor = System.Drawing.Color.White
+        Me.Adminbtn.Location = New System.Drawing.Point(46, 12)
+        Me.Adminbtn.Name = "Adminbtn"
+        Me.Adminbtn.Size = New System.Drawing.Size(124, 20)
+        Me.Adminbtn.TabIndex = 13
+        Me.Adminbtn.Text = "Administration"
+        '
+        'Panel11
+        '
+        Me.Panel11.Location = New System.Drawing.Point(200, 122)
+        Me.Panel11.Name = "Panel11"
+        Me.Panel11.Size = New System.Drawing.Size(1170, 529)
+        Me.Panel11.TabIndex = 4
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(189, 82)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(843, 258)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'AdminLabel
+        '
+        Me.AdminLabel.AutoSize = True
+        Me.AdminLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AdminLabel.ForeColor = System.Drawing.Color.Black
+        Me.AdminLabel.Location = New System.Drawing.Point(206, 71)
+        Me.AdminLabel.Name = "AdminLabel"
+        Me.AdminLabel.Size = New System.Drawing.Size(124, 20)
+        Me.AdminLabel.TabIndex = 14
+        Me.AdminLabel.Text = "Administration"
+        Me.AdminLabel.Visible = False
         '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1370, 703)
-        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Controls.Add(Me.AdminLabel)
         Me.Controls.Add(Me.Panel4)
         Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.ListUserPanel)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Label1)
         Me.Name = "MainForm"
@@ -156,6 +389,21 @@ Partial Class MainForm
         Me.Panel2.ResumeLayout(False)
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel5.ResumeLayout(False)
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel7.ResumeLayout(False)
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel9.ResumeLayout(False)
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel13.ResumeLayout(False)
+        Me.Panel13.PerformLayout()
+        Me.Panel16.ResumeLayout(False)
+        Me.Panel16.PerformLayout()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ListUserPanel.ResumeLayout(False)
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -170,5 +418,25 @@ Partial Class MainForm
     Friend WithEvents Label3 As Label
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents Label2 As Label
-    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents Panel13 As Panel
+    Friend WithEvents Panel16 As Panel
+    Friend WithEvents Adminbtn As Label
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents ListUserPanel As Panel
+    Friend WithEvents PictureBox9 As PictureBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents PictureBox10 As PictureBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Panel9 As Panel
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents Panel10 As Panel
+    Friend WithEvents Panel7 As Panel
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Panel8 As Panel
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents Panel11 As Panel
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents AdminLabel As Label
 End Class
--- AssGroupIII/AssGroupIII/MainForm.vb
+++ AssGroupIII/AssGroupIII/MainForm.vb
@@ -1,3 +1,9 @@
 ﻿Public Class MainForm
+    Private Sub Panel16_Paint(sender As Object, e As PaintEventArgs) Handles Panel16.Paint
+    End Sub
 
+    Private Sub Adminbtn_Click(sender As Object, e As EventArgs) Handles Adminbtn.Click
+        ListUserPanel.Visible = True
+        AdminLabel.Visible = True
+    End Sub
 End Class
