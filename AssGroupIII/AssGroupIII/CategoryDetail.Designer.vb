c98ea2f2d6a56947fe834985a9e80d29f5e4d76c
Se Has <hassecskh@gmail.com>
2021-07-14 22:25:43 +0700

    Add Category Detail form

---
+++ AssGroupIII/AssGroupIII/CategoryDetail.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CategoryDetail
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(CategoryDetail))
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.txtDescription = New System.Windows.Forms.TextBox()
+        Me.txtQty = New System.Windows.Forms.TextBox()
+        Me.LabelAddProductTitle = New System.Windows.Forms.Label()
+        Me.Labeldescription = New System.Windows.Forms.Label()
+        Me.LabelQty = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnCancel
+        '
+        Me.btnCancel.BackColor = System.Drawing.Color.White
+        Me.btnCancel.Image = CType(resources.GetObject("btnCancel.Image"), System.Drawing.Image)
+        Me.btnCancel.Location = New System.Drawing.Point(149, 30)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(90, 42)
+        Me.btnCancel.TabIndex = 54
+        Me.btnCancel.UseVisualStyleBackColor = False
+        '
+        'btnSave
+        '
+        Me.btnSave.Image = CType(resources.GetObject("btnSave.Image"), System.Drawing.Image)
+        Me.btnSave.Location = New System.Drawing.Point(254, 30)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(90, 42)
+        Me.btnSave.TabIndex = 53
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'txtDescription
+        '
+        Me.txtDescription.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDescription.Location = New System.Drawing.Point(143, 143)
+        Me.txtDescription.Multiline = True
+        Me.txtDescription.Name = "txtDescription"
+        Me.txtDescription.Size = New System.Drawing.Size(201, 79)
+        Me.txtDescription.TabIndex = 49
+        '
+        'txtQty
+        '
+        Me.txtQty.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtQty.Location = New System.Drawing.Point(143, 111)
+        Me.txtQty.Name = "txtQty"
+        Me.txtQty.Size = New System.Drawing.Size(201, 26)
+        Me.txtQty.TabIndex = 48
+        Me.txtQty.UseSystemPasswordChar = True
+        '
+        'LabelAddProductTitle
+        '
+        Me.LabelAddProductTitle.AutoSize = True
+        Me.LabelAddProductTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelAddProductTitle.Location = New System.Drawing.Point(10, 30)
+        Me.LabelAddProductTitle.Name = "LabelAddProductTitle"
+        Me.LabelAddProductTitle.Size = New System.Drawing.Size(126, 24)
+        Me.LabelAddProductTitle.TabIndex = 46
+        Me.LabelAddProductTitle.Text = "Add Product"
+        '
+        'Labeldescription
+        '
+        Me.Labeldescription.AutoSize = True
+        Me.Labeldescription.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Labeldescription.Location = New System.Drawing.Point(35, 146)
+        Me.Labeldescription.Name = "Labeldescription"
+        Me.Labeldescription.Size = New System.Drawing.Size(89, 20)
+        Me.Labeldescription.TabIndex = 45
+        Me.Labeldescription.Text = "Description"
+        '
+        'LabelQty
+        '
+        Me.LabelQty.AutoSize = True
+        Me.LabelQty.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelQty.Location = New System.Drawing.Point(33, 117)
+        Me.LabelQty.Name = "LabelQty"
+        Me.LabelQty.Size = New System.Drawing.Size(68, 20)
+        Me.LabelQty.TabIndex = 43
+        Me.LabelQty.Text = "Quantity"
+        '
+        'CategoryDetail
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(396, 330)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.txtDescription)
+        Me.Controls.Add(Me.txtQty)
+        Me.Controls.Add(Me.LabelAddProductTitle)
+        Me.Controls.Add(Me.Labeldescription)
+        Me.Controls.Add(Me.LabelQty)
+        Me.Name = "CategoryDetail"
+        Me.Text = "CategoryDetail"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents txtDescription As TextBox
+    Friend WithEvents txtQty As TextBox
+    Friend WithEvents LabelAddProductTitle As Label
+    Friend WithEvents Labeldescription As Label
+    Friend WithEvents LabelQty As Label
+End Class
